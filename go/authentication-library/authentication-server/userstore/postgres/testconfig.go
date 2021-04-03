8473928cacd5cd913260c8b07374ca8cc29654b4
Mark Standen <no-reply@rundog.dev>
2021-04-03 15:22:57 +0100

    PGTESTHOST added as env var

--- go/authentication-library/authentication-server/userstore/postgres/testconfig.go
+++ go/authentication-library/authentication-server/userstore/postgres/testconfig.go
@@ -1,12 +1,16 @@
 package postgres
 
-import "github.com/markstanden/authentication/deployment/googlecloud"
+import (
+	"os"
+
+	"github.com/markstanden/authentication/deployment/googlecloud"
+)
 
 func GetTestConfig() PGConfig {
 	/*
 		create a new default config but with the dbname "test"
 	*/
-	config := NewConfig().DBName("test").Port("9000").Host("test")
+	config := NewConfig().DBName("test").Port("9000").Host(os.Getenv("PGTESTHOST"))
 
 	/*
 		attempt to to connect to the google secret store (if possible) to retreive secret for production tests
